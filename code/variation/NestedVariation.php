f6bfbd9f0313a7878bbad9876c302cbba7713cbf
Mark Stephens <mark@silverstripe.com>
2013-02-14 16:26:12 +1300

    added nested variation

---
+++ code/variation/NestedVariation.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This variation lets the web site admin nest a scheme. The scenario is perhaps a personalised home page,
+ * where A/B testing is required for one of the personalised options only (e.g. testing response a customer segment).
+ * So for one of the variations, an A/B testing variation invoked.
+ */
+class NestedVariation extends PersonalisationVariation {
+
+	static $has_one = array(
+		"Scheme" => "PersonalisationScheme"
+	);
+
+	function render(ContextProvider $context, Controller $controller = null) {
+		if (!$this->SchemeID) return "";
+		return $this->Scheme()->personalise($controller);
+	}
+
+}

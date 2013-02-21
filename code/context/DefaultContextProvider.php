cb863802347653ec11ab233a418d48a88366a30f
Mark Stephens <mark@silverstripe.com>
2013-02-21 17:24:16 +1300

    added property for logged in member

--- code/context/DefaultContextProvider.php
+++ code/context/DefaultContextProvider.php
@@ -157,6 +157,11 @@ class DefaultContextHandler implements ContextProvider {
 					$v = $this->getLocationProperty($parts);
 					break;
 
+				case "member":
+					$v = $this->getMemberProperty($parts);
+					Debug::show("member prop:" . $v);
+					break;
+
 				default:
 					$v = null;
 			}
@@ -195,6 +200,20 @@ class DefaultContextHandler implements ContextProvider {
 		return null;
 	}
 
+	function getMemberProperty($parts) {
+		if (!isset($parts[1])) return null;
+
+		switch ($parts[1]) {
+			case "loggedin":
+				// Return true if logged in, false if not
+				if (Member::currentUserID()) return "Yes";
+				return "No";
+
+			default:
+				return null;
+		}
+	}
+
 	function getRequestCookie($parts) {
 		if (count($parts) < 2) return null;
 		if (!isset($_COOKIE[$parts[1]])) return null;
@@ -213,6 +232,7 @@ class DefaultContextHandler implements ContextProvider {
 		"request.url"		=> "Text",
 		"cookie.*"			=> "Text",
 		"get.*"				=> "Text",
+		"member.loggedin"	=> "Enum('True,False', 'False')",
 		"location.*"		=> "Text"			// @todo expand location.* properties
 	);
 
